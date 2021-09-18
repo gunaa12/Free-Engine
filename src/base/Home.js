4887b7b01ccead7f499e3d630e38b27ff952fe46
Guna <gunavikasavula@gmail.com>
2021-09-18 15:51:11 -0400

    temp changes trying to integrate search

--- src/base/Home.js
+++ src/base/Home.js
@@ -1,9 +1,18 @@
 import './index.css';
+import CsvReader from './CsvReader'
 
 function Home() {
   return (
     <div id = "home">
-      <p>home</p>
+      <ReactFileReader handleFiles={this.handleFiles} fileTypes={'.csv'}>
+        <button className='btn'>Upload</button>
+      </ReactFileReader>
+      <p>hello there 222</p>
+      <script async src="https://cse.google.com/cse.js?cx=a86591325a0f2c784%22%3E"></script>
+      <div class="gcse-searchboc-only"></div>
+      <output id="out">
+          file contents will appear here
+      </output>
     </div>
   );
 }
