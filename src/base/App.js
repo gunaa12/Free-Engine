ef10b154604e7276c3d5f014a9984ef1f37d4afe
Guna <gunavikasavula@gmail.com>
2021-09-18 23:38:19 -0400

    working version

--- src/base/App.js
+++ src/base/App.js
@@ -9,7 +9,7 @@ class App extends Component {
     render() {
         return (
             <div className = "app">
-                <p>{this.props.name}</p>
+                <p>{this.props.name} -- {this.props.decsription} -- {this.props.alternative}</p>
             </div>
         );
     }
--- src/base/ReadInput.js
+++ src/base/ReadInput.js
@@ -1,54 +1,55 @@
-import React, { useState } from 'react';
-import { useEffect } from 'react';
 import raw from './data.txt';
 import App from './App';
+import { render } from '@testing-library/react';
+import {Component} from 'react';
 
-let data_points;
+export default class ReadInput extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            tokens: [],
+        };
+    }
 
-function ReadInput(){
-    const [csvData, setCsvData] = useState(undefined);
-
-    const [tokens, setToken] = useState([])
-
-
-    useEffect( () => {
+    componentDidMount() {
         fetch(raw)
         .then(r => r.text())
         .then(text => {
+            console.log("async")
             const lines = text.split("\n");
             lines.map((line, index) => {
                 let newtokens = line.split(",");
-                console.log(tokens);
-               setToken([...tokens, newtokens])
+                console.log(newtokens);
+                let temp = this.state.tokens;
+                temp.push(newtokens);
+                this.setState({ tokens: temp });
             })
-        }) 
-    },[])
+        })
+        console.log("useEffect")
+    }
 
-    return(
-        <div>
-            <div className="gcse-search"/>
-            {
-                tokens && tokens.map((tokens) => {
-                    return(
-                        <App name = {tokens}/>
-                    );
-                })
-            }
-            <p>hello</p>
-        </div>
-    );
+    render() {
+        console.log('render')
+        console.log('tokens:' + this.state.tokens)
+        if (this.state.tokens.length === 0) {
+            return(
+                <div></div>
+            );
+        } 
+        else {
+            return(
+                <div>
+                    <div className="gcse-search"/>
+                    {
+                        this.state.tokens.map((words) => {
+                            return (
+                                <App name = {words[0]} description = {words[1]} alternative = {words[2]}/>
+                            );
+                        })
+                    }
+                    <p>hello</p>
+                </div>
+            );
+        }
+    }
 }
-
-// function renderApps() {
-//     fetch(raw).then(r => r.text())
-//     .then(text => {
-//         text.split("\n").map(({line}) => {
-//         console.log(typeof(line));
-//         console.log(line);
-//         const tokens = line.split(",");
-//             <App name = {tokens[0]}/>
-//         });
-//     })
-// }
-
-export default ReadInput;
